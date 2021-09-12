056e893f90f6cc7c870c4bd6ddb2453f12cb35f4
brettsodie <blsodie@gmail.com>
2021-09-12 13:25:01 -0400

    feat: add variable

--- src/services/music/audio/audio.class.ts
+++ src/services/music/audio/audio.class.ts
@@ -21,13 +21,12 @@ export class Audio{
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async create (data: Data, params?: Params): Promise<Data> {
-    console.log(data);
     const id = uuid();
     deleteFile();
     await downloadAudio(data.url);
     await convertToWav();
     await executeCommand(`soundstretch ./public/audio.wav ./public/${id}.wav -pitch=${data.pitch}`);
-    data.url = `http://localhost:3030/${id}.wav`;
+    data.url = `${this.app.get('audioUrl')}/${id}.wav`;
     return data;
   }
 }
